5e6af928f777c8a42bc38606bb24576e32bd1a6d
Thong Dinh <tdinh18@berkeley.edu>
2016-04-08 21:36:03 -0700

    can upload photos, not showing though

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,9 @@
 class Post < ActiveRecord::Base
-  has_attached_file :image, :styles => { :small => "150x150>" },
-                    :url  => "/assets/products/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+	has_attached_file :image, :styles => { :small => "150x150>" },
+	                  url: "/system/:hash.:extension",
+	                  hash_secret: "abc123"
 
-  validates_attachment_presence :image
-  validates_attachment_size :image, :less_than => 5.megabytes
-  validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
-
+	validates_attachment_presence :image
+	validates_attachment_size :image, :less_than => 5.megabytes
+	validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
 end
