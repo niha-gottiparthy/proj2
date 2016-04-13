89fc20ef760e92a94417ab2005900a0a06774639
Thong Dinh <tdinh18@berkeley.edu>
2016-04-12 23:19:20 -0700

    Fixed login views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 	devise_scope :user do
-	  get "/login" => "devise/sessions#new"
+	  get "/auth/login" => "devise/sessions#new"
 	end
   resources :post
   devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
