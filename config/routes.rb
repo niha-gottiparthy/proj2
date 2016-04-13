82b43946d66471bff637a9affd6fc5f4ba30d1ed
Allen Wang <allen.y.w@berkeley.edu>
2016-04-12 19:43:00 -0700

    Set up account registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+	devise_scope :user do
+	  get "/login" => "devise/sessions#new"
+	end
   resources :post
-  devise_for :users
+  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   get 'posts/new', to: 'posts#new', as: 'new'
   get 'home/index', to: 'home#index', as: 'home'
   post 'posts', to: 'posts#create'
