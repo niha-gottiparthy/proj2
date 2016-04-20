f1155609c35c061db31e2ab8478c750ae20e5656
Thong Dinh <tdinh18@berkeley.edu>
2016-04-19 18:12:43 -0700

    tweaked devise views and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-	devise_scope :user do
-	  get "/auth/login" => "devise/sessions#new"
-	end
   resources :post
-  devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  get 'posts/new', to: 'posts#new', as: 'new'
+  devise_for :users
+  resources :users
   get 'home/index', to: 'home#index', as: 'home'
   post 'posts', to: 'posts#create'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160420001746) do
+ActiveRecord::Schema.define(version: 20160420010444) do
 
 # Could not dump table "posts" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160420001746) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.integer  "privilege"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
